31e15a520947ae5e2e68a169ab01f7227dea43c7
Kimberly Acquilano <97560083+kacquilano1@users.noreply.github.com>
2026-06-25 11:59:25 -0500

    added main_test.py

---
+++ stl-meetings/app/main_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import os, re, sqlite3, hashlib, smtplib, logging, threading, time
+os.environ["DYLD_LIBRARY_PATH"] = "/opt/homebrew/lib"
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import datetime
+from pathlib import Path
+from urllib.parse import urljoin, urlparse, quote
+import requests
+from icalendar import Calendar
+from bs4 import BeautifulSoup
+from weasyprint import HTML
+from flask import Flask, render_template, request, jsonify
+import schedule
+
+# --This is the new file
+# Nothing's here, it's fine
